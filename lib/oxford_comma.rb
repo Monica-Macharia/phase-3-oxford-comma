3736c3f7a95d67ea886fd8e5e2d5b207ddb6a66f
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-08-20 12:04:03 +0300

    commit tree d9e690d778d31a02ed3a1464b40219c32765b036

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,34 @@
+# def oxford_comma(array)
+#     if array.length == 1
+#    one = array.to_s
+#     else
+#         nil
+#     end
+#     one
+# end
+# def oxford_comma(array)
+#     adds= array.join(" ""and"" ")
+#     adds
+# end
+# # def oxford_comma(array)
+# #     if array.length < 2
+# #         first = array.join(",")
+# #         first
+# #       elsif array.length = 3
+# #         second = array[1,2].join(",""and")
+# #          second  
+# #        end
+# #        "first" + "second"
+# # end
+# def oxford_comma(array)
+# end
 def oxford_comma(array)
-
+    if array.length == 1
+        return "#{array[0]}"
+    elsif array.length == 2
+        return array.join(" ""and"" ")
+    elsif array.length >= 3 
+        array[-1] = "and #{array[-1]}"
+        return array.join(", ")
+    end
 end
